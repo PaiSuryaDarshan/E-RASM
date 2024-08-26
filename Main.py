21cf2498712bd9a28b3577e3362236f6bb7c8bf2
PaiSuryaDarshan <paisuryadarshan.research@gmail.com>
2024-08-26 22:54:38 +0530

    update Main.py

--- Main.py
+++ Main.py
@@ -10,3 +10,6 @@ for chemical in list_of_chemicals:
     url             = P1.GRAB_SDS_URL(CHEMICAL)
     file_path       = P1.URL_to_PDF(url, CHEMICAL)
     CHEMICAL_NAME   = P1.obtain_name(file_path, CHEMICAL)
+
+    # P2 - SDS Parsing
+    
