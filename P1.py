d15a1a2735c855a5630ebe18142108e8436165ac
PaiSuryaDarshan <paisuryadarshan.research@gmail.com>
2024-08-26 14:35:34 +0530

    feat: create P1.py and sys_checks.py

---
+++ P1.py
@@ -0,0 +1,7 @@
+# Un-Licensed. Free to use, copy, modify and merge.
+# Please maintain attribution to the original author and the license.
+
+import sys
+import playwright 
+
+print(sys.platform)
---
+++ sys_checks.py
@@ -0,0 +1,22 @@
+
+import sys
+import platform
+
+OS_Flag = ''
+release = platform.system() + ' ' + platform.release()
+version = platform.version()
+
+# Returns OS information and other prerequisites that may be required 
+# to make suitable adaptions
+
+if sys.platform == 'win32':
+    OS_Flag = "Windows"
+
+if sys.platform == 'darwin':
+    OS_Flag = "Mac OS X"
+
+print (f'''
+{OS_Flag} detected
+release: {release}
+version: {version}
+''')
