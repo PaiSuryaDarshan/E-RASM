eca7bd51ed06cac111b7b3290716c3a37d5d9361
PaiSuryaDarshan <paisuryadarshan.research@gmail.com>
2024-08-26 17:57:18 +0530

    update 1 file and create 1 file

--- P1.py
+++ P1.py
@@ -3,28 +3,27 @@
 
 import sys_checks
 from playwright.sync_api import sync_playwright
+import requests
 
 CHEMICAL = "ethanol".capitalize()
 
 # Function that obtains SDS PDF download link
-def SDS_URL_GRABBER(url):
+def GRAB_SDS_URL(CHEMICAL):
     WEBSITE = "https://www.sigmaaldrich.com"
     SEARCH_BUTTON_ID = "header-search-submit-search-wrapper"
     with sync_playwright() as p:
 
-    # First Step: Find chemical
-
         browser = p.chromium.launch(headless=False)
         page    = browser.new_page()
         page.goto(WEBSITE)
+
+        # Find chemical
         page.get_by_role("textbox", name="Type in Product").fill(CHEMICAL)
         page.get_by_test_id(SEARCH_BUTTON_ID).click()
 
         # Using page.locator('element[object-property='value']') to find precise match. 
         # Using .nth(0) to select the first button - Index = '0'
         page.locator('button[aria-describedby="sds"]').nth(0).click()
-
-    # Second Step: Obtain SDS link
 
         # Grabbing link for English version
         href_tag = page.get_by_test_id("sds-link-EN").get_attribute('href')
@@ -33,4 +32,13 @@ def SDS_URL_GRABBER(url):
         final_download_link = WEBSITE + href_tag
         page.close()
 
-    # Third Step: Download SDS
+    return final_download_link
+
+# Function to Download SDS to a specific directory
+def URL_to_PDF(url):
+    with open(f"./storage_directory/{CHEMICAL}.pdf", "wb") as f:
+        f.write(requests.get(url))
+        f.close()
+
+url = GRAB_SDS_URL(CHEMICAL)
+URL_to_PDF(url)
