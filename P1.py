4cf0c5058e41d83b399392a914a9be5a6ca5cd1a
PaiSuryaDarshan <paisuryadarshan.research@gmail.com>
2024-08-26 17:44:14 +0530

    update settings.json and P1.py

--- P1.py
+++ P1.py
@@ -4,20 +4,33 @@
 import sys_checks
 from playwright.sync_api import sync_playwright
 
-WEBSITE = "https://www.sigmaaldrich.com/"
-CHEMICAL = "methyl alcohol".capitalize()
+CHEMICAL = "ethanol".capitalize()
 
-# First Step: Accessing WEBSITE
+# Function that obtains SDS PDF download link
+def SDS_URL_GRABBER(url):
+    WEBSITE = "https://www.sigmaaldrich.com"
+    SEARCH_BUTTON_ID = "header-search-submit-search-wrapper"
+    with sync_playwright() as p:
 
-with sync_playwright() as p:
+    # First Step: Find chemical
 
-    browser = p.chromium.launch(headless=False, slow_mo=1000)
-    page    = browser.new_page()
-    page.goto(WEBSITE)
-    page.get_by_role("textbox", name="Type in Product").fill(CHEMICAL)
-    page.get_by_test_id("header-search-submit-search-wrapper").click()
+        browser = p.chromium.launch(headless=False)
+        page    = browser.new_page()
+        page.goto(WEBSITE)
+        page.get_by_role("textbox", name="Type in Product").fill(CHEMICAL)
+        page.get_by_test_id(SEARCH_BUTTON_ID).click()
 
-# Second Step: Checking the search results
+        # Using page.locator('element[object-property='value']') to find precise match. 
+        # Using .nth(0) to select the first button - Index = '0'
+        page.locator('button[aria-describedby="sds"]').nth(0).click()
 
-    page.get_by_role("button", name="sds").click()
-    page.kis
+    # Second Step: Obtain SDS link
+
+        # Grabbing link for English version
+        href_tag = page.get_by_test_id("sds-link-EN").get_attribute('href')
+
+        # Constructing final download link by concatenating the website URL and the href tag
+        final_download_link = WEBSITE + href_tag
+        page.close()
+
+    # Third Step: Download SDS
