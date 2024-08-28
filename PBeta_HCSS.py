a3840cfa2a17751a032d7126576fab304efd1483
PaiSuryaDarshan <paisuryadarshan.research@gmail.com>
2024-08-28 16:39:54 +0530

    update 4 files and create 1 file

--- PBeta_HCSS.py
+++ PBeta_HCSS.py
@@ -105,30 +105,106 @@ def process_hazards(hazards_list: str) -> list[bool]:
     ]
 
     for index, flag in enumerate(interpreted_flags):
+        # Define ROE variable (stores ROE code)
+        ROE_value = 0
+        interpreted_ROE.append(ROE_value)
+        interpreted_flags[index] = 0
+
         # iterates each item in list looking for keywords
         for hazard in hazards_list:
+
             flag, ROE_value = haz_flag(hazard, locators[index], flag)
             if flag:
-                print(index, ROE_value)
+                # print(index, ROE_value)
+                # Record interpretation
+                interpreted_flags[index] = 1
+                interpreted_ROE[index] += ROE_value
             else:
                 pass
+        
+            # reset flags to default (False) before next iteration / cycle
+            flag = False
+
+
 
     return interpreted_flags, interpreted_ROE
 
 
+# ----------------------------------------------------------------
+# TEST FUNCTIONS
+# ----------------------------------------------------------------
 
-# TEST
+def prior_information():
+    print(("""
+Index labels
+    0: carcinogen
+    1: teratogen
+    2: mutagen
+    3: toxic
+    4: harmful
+    5: irritant
+    6: explosive
+    7: pyrophoric
+    8: flammable
+    9: oxidising
+    10:corrosive
+    11:lachrymator
+        
+ROE_values (possible interpretations):
+    Simple value:
+        101: Inhalation
+        102: Skin/Eye
+        103: Swallow
+    Complex value: (Arises out of a combination of routes)
+        203: Inhalation + Skin/Eye
+        204: Inhalation + Swallow
+        205: Skin/Eye   + Swallow
+    Total value:
+        306: All possible routes (Inhalation, Skin/Eye, Swallow)
+""").upper())
 
-hazards_list = [
-    'h225  highly flammable liquid and vapor.', # 0
-    'h304  may be fatal if swallowed and enters airways.', # 103
-    'h315  causes skin irritation.', # 102
-    'h336  may cause drowsiness or dizziness.', # FALSE
-    'h361d  suspected of damaging the unborn child.', # FALSE
-    'h373  may cause damage to organs (central nervous system) through prolonged or repeated exposure if inhaled.', # FALSE
-    'h412  harmful to aquatic life with long lasting effects.' # FALSE
-    ]
+def test():
+    # Sample values with expected results (INDEX, ROE_value)
+    hazards_list_1 = [
+        'h225  highly flammable liquid and vapor.', # (8, 0)
+        'h319  causes serious eye irritation.' # (5, 102)
+        ]
 
-flag_list, ROE_list = process_hazards(hazards_list)
+    hazards_list_2 = [
+        'h225  highly flammable liquid and vapor.', # (8, 0)
+        'h301 + h311 + h331  toxic if swallowed, in contact with skin or if inhaled.', # (3, 306)
+        'h370  causes damage to organs (eyes, central nervous system).' #! (Null)
+        ]
 
-print(len(ROE_list))
+    hazards_list_3 = [
+        'h225  highly flammable liquid and vapor.', # (8, 0)
+        'h304  may be fatal if swallowed and enters airways.', 
+        'h315  causes skin irritation.', # (5, 102)
+        'h336  may cause drowsiness or dizziness.',  #! (Null)
+        'h361d  suspected of damaging the unborn child.',  #! (Null)
+        'h373  may cause damage to organs (central nervous system) through prolonged or repeated exposure if inhaled.',  #! (Null)
+        'h412  harmful to aquatic life with long lasting effects.'# (4, 0)
+        ]
+    
+    hazards_list_4 = [
+        'h317  may cause an allergic skin reaction.',  #! (Null)
+        'h373  may cause damage to organs through prolonged or repeated exposure.',  #! (Null)
+        'h410  very toxic to aquatic life with long lasting effects.' # (3, 0)
+        ]
+
+    flag_list, ROE_list = process_hazards(hazards_list_1)
+
+    print(flag_list)
+    print(ROE_list)
+    print()
+
+    counter = 0
+    for i in flag_list:
+        if i == True:
+            print(f"hazard found at {counter} with route {ROE_list[counter]}.")
+        counter += 1 
+
+if __name__ == "__main__":
+    prior_information()
+    # test()
+    pass
